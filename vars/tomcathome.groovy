83445bb90eaf450c7353b9b56afba7f08d8c9cf2
suni4478 <110048914+suni4478@users.noreply.github.com>
2022-08-28 00:04:31 +0530

    Update tomcathome.groovy

--- vars/tomcathome.groovy
+++ vars/tomcathome.groovy
@@ -1,8 +1,9 @@
-def call(ip,id)
+def call(ip,id){
 sshagent(['id']) {
 sh "mv target/sunil*.war target/sunil.war"
                                     // war file
 sh "scp -o StrictHostKeyChecking=no target/sunil.war ec2-user@${ip}:/opt/tomcat9/webapps"
 sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
 sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
+}
 }
