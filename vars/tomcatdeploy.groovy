4df33aa21b9af060160264a79952a66f6941989b
suni4478 <110048914+suni4478@users.noreply.github.com>
2022-11-16 16:48:17 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,5 +1,5 @@
 def call(ip,username,id) { 
-   sshagent(['id']) {
+   sshagent([id]) {
    sh "mv target/onlinebookstore*.war target/app.war"
      sh "scp -o StrictHostKeyChecking=no target/app.war ${username}@${ip}:/opt/tomcat9/webapps"
      sh "ssh -o StrictHostKeyChecking=no ${username}@${ip} /opt/tomcat9/bin/shutdown.sh"
