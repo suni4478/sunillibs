c0c6543448465e3d2967a0b226e98da18aa1aff0
suni4478 <110048914+suni4478@users.noreply.github.com>
2022-11-16 16:35:30 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,9 +1,8 @@
 def call(ip,username,id) { 
-  sshagent([id]) {
-  sh "mv target/sunil*.war target/app.war"
-  // some block
-  sh "scp -o StrictHostKeyChecking=no target/app.war ${username}@${ip}:/opt/tomcat10/webapps"
-  sh "ssh ${username}@${ip} /opt/tomcat10/bin/shutdown.sh"
-  sh "ssh ${username}@${ip} /opt/tomcat10/bin/startup.sh"
+   sshagent(['id']) {
+   sh "mv target/onlinebookstore*.war target/app.war"
+     sh "scp -o StrictHostKeyChecking=no target/app.war ${username}@${ip}:/opt/tomcat9/webapps"
+     sh "ssh -o StrictHostKeyChecking=no ${username}@{ip} /opt/tomcat9/bin/shutdown.sh"
+     sh "ssh -o StrictHostKeyChecking=no ${username}@{ip} /opt/tomcat9/bin/startup.sh"
   }
 }
